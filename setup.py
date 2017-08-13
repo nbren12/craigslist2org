f61c5adfd93ed27454fab8fa6045f1944cb54644
Noah D. Brenowitz <nbren12@gmail.com>
2017-08-13 12:03:53 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='craigslist2org',
+      version='0.0',
+      description='Craigslist to Emacs org-mode parser',
+      author='Noah Brenowitz',
+      author_email='nbren12@gmail.com',
+      scripts = ['craigslist2org.py'])

f34ff87bdba44e2b51c69276aa8725a66bf69a54
Noah D. Brenowitz <nbren12@gmail.com>
2017-08-13 12:07:43 -0700

    add install_requires

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from distutils.core import setup
 
+install_requires = [
+    'docopt',
+    'lxml',
+    'beautifulsoup4']
+
 setup(name='craigslist2org',
       version='0.0',
       description='Craigslist to Emacs org-mode parser',
       author='Noah Brenowitz',
       author_email='nbren12@gmail.com',
-      scripts = ['craigslist2org.py'])
+      scripts = ['craigslist2org.py'],
+      install_requires=install_requires)
